1b8472fe4e431b521a75e56b63116f772674d90b
Geoffrey Kimani <geoffrey.kimani@sbsafrica.com>
2018-05-10 10:31:20 +0300

    Set up the application

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/routes'
+
+//Jquery and Bootstrap
+import jQuery from 'jquery'
+global.jQuery = jQuery
+global.$ = jQuery
+let Bootstrap = require('bootstrap')
+import 'bootstrap/dist/css/bootstrap.css'
+
+//Vue Settings
+Vue.config.productionTip = false
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/router/routes.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+//Components
+import HomePage from '@/components/home'
+
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'HomePage',
+            component: HomePage
+        },
+    ]
+})
